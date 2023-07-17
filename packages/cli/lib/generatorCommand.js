378c51b450d5cbeede6cceb6683bc11a958386a3
zcxiaobao <444186976@qq.com>
2023-07-17 15:54:21 +0800

    添加对命令的监听

--- packages/cli/lib/generatorCommand.js
+++ packages/cli/lib/generatorCommand.js
@@ -6,7 +6,7 @@ import semver from "semver";
 import { program } from "commander";
 import { log } from "@zc-clis/utils";
 
-const LOWEST_NODE_VERSION = "19.0.0";
+const LOWEST_NODE_VERSION = "18.0.0";
 
 const __dirname = dirname(import.meta);
 const pkgPath = path.join(__dirname, "../package.json");
@@ -32,5 +32,13 @@ export default function generatorCommand() {
     .hook("preAction", () => {
       checkNodeVersion();
     });
+  program.on("command:*", function (obj) {
+    log.error("未知的命令 " + obj[0]);
+  });
+  program.on("option:debug", function () {
+    if (program.opts().debug) {
+      log.verbose("debug", "launch debug mode!");
+    }
+  });
   return program;
 }
