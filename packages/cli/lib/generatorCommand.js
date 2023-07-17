b561b58bab7ea1f82980bade26684f663a143da1
zcxiaobao <444186976@qq.com>
2023-07-17 15:16:44 +0800

    ESM对dirname和pkg的获取

--- packages/cli/lib/generatorCommand.js
+++ packages/cli/lib/generatorCommand.js
@@ -1,17 +1,21 @@
-import { program } from "commander";
-export default function generatorCommand() {
-  program
-    .name("zcli")
-    .usage("<command> [options]")
-    .version("1.0.0")
-    .option("-d, --debug", "是否开启调试模式", false);
+import path from "node:path";
+import fsExtra from "fs-extra";
+import { dirname } from "dirname-filename-esm";
 
-  program.on("option:debug", function () {
-    console.log(program.opts());
-    if (program.opts().debug) {
-      //   log.verbose("debug", "launch debug mode");
-    }
-  });
+import { program } from "commander";
+import { log } from "@zc-clis/utils";
+
+const __dirname = dirname(import.meta);
+const pkgPath = path.join(__dirname, "../package.json");
+const pkg = fsExtra.readJSONSync(pkgPath);
+export default function generatorCommand() {
+  const pkg = fsExtra.readJSONSync(pkgPath);
+  log.info("version", pkg.version);
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage("<command> [options]")
+    .version(pkg.version)
+    .option("-d, --debug", "是否开启调试模式", false);
 
   return program;
 }
