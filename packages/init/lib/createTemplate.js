6679f50b08be3ebd54114d06ea7b5d70361a2e77
zcxiaobao <444186976@qq.com>
2023-07-18 15:21:49 +0800

    createTemplate 基础配置

---
+++ packages/init/lib/createTemplate.js
@@ -0,0 +1,26 @@
+const ADD_TEMPLATE = [
+  {
+    name: "vue3项目模板",
+    npmName: "@zcxiaobao/template-vue3",
+    version: "1.0.0",
+  },
+  {
+    name: "react18项目模板",
+    npmName: "@zcxiaobao/template-react18",
+    version: "1.0.0",
+  },
+];
+
+const ADD_TYPE = {
+  PAGE: "PAGE",
+  PROJECT: "PROJECT",
+};
+
+const ADD_TYPE_LIST = [
+  { name: "项目", value: ADD_TYPE.PROJECT },
+  { name: "页面", value: ADD_TYPE.PAGE },
+];
+
+export default function createTeplate(name, opts) {
+  // 获取创建类型
+}
--- packages/init/lib/index.js
+++ packages/init/lib/index.js
@@ -1,5 +1,6 @@
 import Command from "@zcxiaobao/command";
 import { log } from "@zcxiaobao/utils";
+import createTeplate from "./createTemplate.js";
 class InitCommand extends Command {
   get command() {
     return "init [name]";
@@ -15,6 +16,10 @@ class InitCommand extends Command {
 
   action([name, opts]) {
     log.verbose("init", name, opts);
+    // 1. 选择项目模板，生成项目信息
+    createTeplate(name, opts);
+    // 2. 下载项目模板至缓存目录
+    // 3. 安装项目模板至项目目录
   }
 }
 
