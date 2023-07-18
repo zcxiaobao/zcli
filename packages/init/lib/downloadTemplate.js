6a9a57e4e2de9600d99bf12fbdf17f0762468000
zcxiaobao <444186976@qq.com>
2023-07-18 17:09:59 +0800

    downloadTemplate：将模板下载到缓存目录

--- packages/init/lib/downloadTemplate.js
+++ packages/init/lib/downloadTemplate.js
@@ -1,1 +1,37 @@
-export default function downloadTemplate(template) {}
+import path from "node:path";
+import fsExtra from "fs-extra";
+import { execa } from "execa";
+import ora from "ora";
+import { pathExistsSync } from "path-exists";
+import { printErrorLog, log } from "@zcxiaobao/utils";
+function getCacheDir(targetPath) {
+  return path.resolve(targetPath, "node_modules");
+}
+
+function makeCacheDir(targetPath) {
+  const cacheDir = getCacheDir(targetPath);
+  if (!pathExistsSync(cacheDir)) {
+    fsExtra.mkdirpSync(cacheDir);
+  }
+}
+
+async function downloadAddTemplate({ npmName, version }, targetPath) {
+  const installCommand = "npm";
+  const installArgs = ["install", `${npmName}@${version}`];
+  const cwd = targetPath;
+  log.verbose("cwd", cwd);
+  log.verbose("installArgs", installArgs);
+  await execa(installCommand, installArgs, { cwd });
+}
+export default async function downloadTemplate({ template, targetPath }) {
+  makeCacheDir(targetPath);
+  const spinner = ora("正在下载模板...").start();
+  try {
+    await downloadAddTemplate(template, targetPath);
+    spinner.stop();
+    log.success("下载模板成功");
+  } catch (e) {
+    spinner.stop();
+    printErrorLog(e);
+  }
+}
--- packages/init/lib/index.js
+++ packages/init/lib/index.js
@@ -18,9 +18,9 @@ class InitCommand extends Command {
   async action([name, opts]) {
     // log.verbose("init", name, opts);
     // 1. 选择项目模板，生成项目信息
-    const { template } = await createTeplate(name, opts);
+    const selectedTemplate = await createTeplate(name, opts);
     // 2. 下载项目模板至缓存目录
-    await downloadTemplate(template);
+    await downloadTemplate(selectedTemplate);
     // 3. 安装项目模板至项目目录
   }
 }
